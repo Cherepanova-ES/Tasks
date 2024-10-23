b7c04cfd20530a2470aa1e0433de2a5647052b8f
Cherepanova-ES <ekatbobo@gmail.com>
2024-10-23 12:46:22 +0300

    done

--- src/task1/AnimalManager.java
+++ src/task1/AnimalManager.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-
+//вариант 5//
 
 public class AnimalManager {
     private List<Animal> animals = new ArrayList<>();
